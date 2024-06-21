253ca730e4e00e572bd30647d7df00bb1ca369a7
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-21 10:24:31 +0530

    first commit

---
+++ login-registration-springboot-hibernate-jsp-auth/src/main/java/com/login/LoginRegistrationSpringbootHibernateJspAuthApplication.java
@@ -0,0 +1,13 @@
+package com.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationSpringbootHibernateJspAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationSpringbootHibernateJspAuthApplication.class, args);
+	}
+
+}
