b6f7c605e78f350c46642e87eee6193c8474d6f6
Chetan Malabade <malabade.chetan@gmail.com>
2024-12-04 13:40:09 +0530

    spring boot webflux

---
+++ springboot-webflux-demo/src/main/java/com/alien/service/CustomerService.java
@@ -0,0 +1,38 @@
+package com.alien.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alien.dao.CustomerDao;
+import com.alien.dto.Customer;
+
+import reactor.core.publisher.Flux;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerDao dao;
+
+	public List<Customer> loadAllCustomers() {
+		long start = System.currentTimeMillis();
+		List<Customer> customers = dao.getCustomers();
+		long end = System.currentTimeMillis();
+
+		System.out.println("Total execution time : " + (end - start));
+
+		return customers;
+	}
+
+	public Flux<Customer> loadAllCustomersStream() {
+		long start = System.currentTimeMillis();
+		Flux<Customer> customers = dao.getCustomersStream();
+		long end = System.currentTimeMillis();
+
+		System.out.println("Total execution time : " + (end - start));
+		return customers;
+	}
+
+}
