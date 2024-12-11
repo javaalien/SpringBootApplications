9ed4655e4b0e96248ba77c31aba035e150f23732
Chetan Malabade <malabade.chetan@gmail.com>
2024-12-11 10:05:52 +0530

    spring boot multithreading

---
+++ springboot-multithreading-example/src/main/java/com/alien/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.alien.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alien.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
